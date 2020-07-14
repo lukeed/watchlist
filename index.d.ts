0fac39a61479b43bbe28a640ac224b6c82c36571
Luke Edwards <luke.edwards05@gmail.com>
2020-07-14 00:31:17 -0700

    chore(types): import `Arrayable` type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
+type Arrayable<T> = T[] | T;
+
 export interface Options {
 	cwd: string;
-	ignore: Array<RegExp | string>;
+	ignore: Arrayable<RegExp | string>;
 }
 
 export function watch(dirs: string[], command: string, opts?: Partial<Options>): Promise<void>;
